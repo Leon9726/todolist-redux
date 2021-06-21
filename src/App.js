c1ecb6a0180f6627721f94052fefe7281533d32d
Leonardo <leon-97@live.it>
2021-06-21 12:34:04 +0200

    impl redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { createStore } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let todos = ["Fare la spesa",
+"Fare i compiti",
+"Uscire il cane"]
+
+function storeReducer(state = {}, action)  {
+  return state;
+}
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    const store = createStore(storeReducer, {todos: [...todos]});
+    this.setState( {todos: [...store.getState().todos]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>TO DO LIST</h1>
+        </header>
+        <ul>
+            {
+              this.state.todos.map( (todo,i) => <li key={i}>{todo}</li>)
+            }
+          </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
